f2411e29117b0de8a1cafe11c49c5f77e69035bb
Abid Hussain <abidhussain3008@gmail.com>
2019-09-18 01:48:51 +0530

    Add reactstrap and add navbar

---
+++ src/components/navigationBar/index.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import "./style.css";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink
+} from 'reactstrap';
+
+class NavigationBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="white" light expand="md">
+          <NavbarBrand href="/">TechnoMelange</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink >Home</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink >Theme</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink >Event</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+        { this.props.children  }
+      </div>
+    );
+  }
+}
+
+export default NavigationBar;

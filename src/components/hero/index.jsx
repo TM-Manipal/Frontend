b9934438930d2e512fe539c361eb4702a70909f5
Adarsh <adarshbhalotia2910@gmail.com>
2019-09-23 02:57:23 +0530

    UI adjustments for pacman

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -12,7 +12,7 @@ export default class Hero extends React.Component {
             <Col md={{ size: 6, offset: 3 }}>
               <img className="tm-image" src={TechnoImg} alt="" />
             </Col>
-            <Col md={{size: 6, offset: 3}} className="pacman-holder">
+            <Col md={{size: 4, offset: 4}} className="pacman-holder">
               <div className="pacman"></div>
             </Col>
           </div>

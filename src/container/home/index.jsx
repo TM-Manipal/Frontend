060bdbff6cb70904cea1b22a5cd050d829881bf2
Abid Hussain <abidhussain3008@gmail.com>
2019-09-23 00:58:31 +0530

    add inquizitive logo

--- src/container/home/index.jsx
+++ src/container/home/index.jsx
@@ -25,7 +25,7 @@ export default class Home extends React.Component {
         name : "Gaming",
       },
       {
-        name : "Capture The Flag",
+        name : "Inquizitive",
       },
       {
         name : "Mad Ads",

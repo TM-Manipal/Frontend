ca5798f176375fad72c4deeffcb0f4ab87e688ae
Abid Hussain <abidhussain3008@gmail.com>
2019-09-27 00:43:23 +0530

    Remove posters

--- src/container/event/index.jsx
+++ src/container/event/index.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Card from '../../components/card';
-import _ from 'lodash';
 import {
  Container, 
 } from 'reactstrap';
@@ -11,7 +10,6 @@ export default class Event extends React.Component {
     super(props);
     this.state = {
       events: [],
-      modal: false,
     }
   }
 
@@ -26,55 +24,6 @@ export default class Event extends React.Component {
         },
         (error) => {
           console.log({"err" : error})
-        }
-      )
-  }
-
-  getEventById = (id) => {
-    return this.state.events.filter(
-        function(event){ return event.id === id }
-    );
-  }
-
-  toggle = (id) => {
-    this.setState(prevState => ({
-      modal: !prevState.modal,
-      currentEvent: this.getEventById(id)[0]
-    }));
-  }
-
-  onFormSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.target);
-
-    this.payload = {
-      "college": data.get('college'),
-      "participants": _.times(this.state.currentEvent.maxMembersPerTeam, (i) => {
-        return({
-          name: data.get('name'+i),
-          email: data.get('email'+i),
-          mobile: data.get('phno'+i)
-        })
-      })
-    }
-    console.log(this.payload);
-    //make post to server
-    fetch("http://"+ process.env.REACT_APP_API_URL + ":" + process.env.REACT_APP_API_PORT +"/events/"+ this.state.currentEvent.id+"/register", {
-      method: "post",
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(this.payload)
-    })
-      .then(res => res.json())
-      .then(
-        async (result) => {
-          alert("Registration Successful");  
-          this.toggle();
-        },
-        (error) => {
-          alert("Registration Failed");  
         }
       )
   }
